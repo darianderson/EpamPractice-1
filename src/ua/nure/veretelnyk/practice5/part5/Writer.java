312d945ac49d52e91a7e397c628f715bab1b41c5
dmitryblackwell <dmitryblackwell@gmail.com>
2018-05-04 19:23:18 +0300

    Practice 6. Task 5.
    Finish task with 10 threads, that write there number to one file using RandomAccessFile. It is really awesome.

--- src/ua/nure/veretelnyk/practice5/part5/Writer.java
+++ src/ua/nure/veretelnyk/practice5/part5/Writer.java
@@ -1,26 +1,39 @@
 package ua.nure.veretelnyk.practice5.part5;
 
 import java.io.IOException;
+import java.io.RandomAccessFile;
 
 public class Writer extends Thread{
-    private static final int COUNTS = 20;
     private int num;
+    private final RandomAccessFile raf;
+    private long pointer;
+    private static final long STRING_LENGTH = 20L + System.lineSeparator().length();
 
-    public Writer(int num) {
+    Writer(int num, RandomAccessFile raf){
         this.num = num;
+        this.raf =raf;
+        pointer = num*STRING_LENGTH;
     }
 
     @Override
     public void run() {
-        try {
-            RAF raf = RAF.getInstance();
-            for(int i=0; i<COUNTS; ++i){
-                raf.goTo(num);
-                raf.write(String.valueOf(i));
+        for(int i=0; i<20; ++i){
+            synchronized (raf){
+                try{
+                    raf.seek(pointer+i);
+                    raf.writeBytes(String.valueOf(num));
+                }catch (IOException e){
+                    e.printStackTrace();
+                }
             }
-        } catch (IOException e) {
-            e.printStackTrace();
         }
-
+        synchronized (raf){
+            try{
+                raf.seek(pointer + 20);
+                raf.write(System.lineSeparator().getBytes());
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+        }
     }
 }
--- src/ua/nure/veretelnyk/practice5/part5/part5.java
+++ src/ua/nure/veretelnyk/practice5/part5/part5.java
@@ -4,18 +4,37 @@ import java.io.*;
 import java.util.Scanner;
 
 public class part5 {
+
     public static void main(String[] args) throws IOException, InterruptedException {
+        final String path = "data/practice5/part5.txt";
 
-        Writer[] writer = new Writer[10];
-        for(int i=0; i<10; ++i){
-            writer[i] = new Writer(i);
-            writer[i].start();
-        }
-        for (int i=0; i<10; ++i)
-            writer[i].join();
+        File file = new File(path);
+        boolean isDeleted = false;
+        if (file.exists())
+            isDeleted = file.delete();
+        boolean isCreated = file.createNewFile();
+        if (!isCreated && !isDeleted)
+            System.out.println("File wasn\'t created or deleted.");
 
-        Scanner scanner = new Scanner(new File(RAF.FILE_PATH), "UTF-8");
-        while (scanner.hasNextLine())
-            System.out.println(scanner.nextLine() + "\n");
+        RandomAccessFile raf = new RandomAccessFile(file,"rw");
+        Thread[] threads = new Thread[10];
+
+        for(int i=0; i<threads.length; ++i)
+            threads[i] = new Writer(i, raf);
+
+        for (Thread t : threads)
+            t.start();
+
+
+        for (Thread t : threads)
+            t.join();
+
+        raf.close();
+
+        Scanner in = new Scanner(new File(path), "UTF-8");
+        while (in.hasNextLine())
+            System.out.println(in.nextLine());
+
+        in.close();
     }
 }
